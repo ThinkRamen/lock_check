d3e23c42e514dc4bdd3832e65c7d56ca0bf87f88
Alejandro <ThinkRamen@gmail.com>
2023-01-11 18:23:28 -0800

    reduced repitition of Popen command

--- lock_check.py
+++ lock_check.py
@@ -4,15 +4,20 @@ import subprocess
 default_password = 'admin'
 
 
+def output_cmd(cmd):
+    proc = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = proc.communicate()[0]
+    output = output.decode("utf-8").strip()
+    return output
+
+
 def serial_number():
     """
     returns serial number from system command using subprocess.Popen, formats the data from bytes to str, and formats it.
     """
     check_serial = "system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'"
-    proc = subprocess.Popen(
-        check_serial, shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    serial_number = output.decode("UTF-8").strip()
+    serial_number = output_cmd(check_serial)
     return serial_number
     ###
 
@@ -22,9 +27,7 @@ def fmm_status():
     checks for find my mac.
     """
     check_fmm = 'nvram -p | awk "/fmm-mobileme-token-FMM-BridgeHasAccount/{print $NF}"'
-    proc = subprocess.Popen(check_fmm, shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    fmm_status = output.decode("UTF-8").strip()
+    fmm_status = output_cmd(check_fmm)
     if fmm_status == 'BridgeHasAccountValue':
         fmm_status = True
     else:
@@ -35,50 +38,36 @@ def fmm_status():
 
 def icloud_status():  # TODO
     check_icloud = ''
-    proc = subprocess.Popen(check_icloud,
-                            shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    output = output.decode("UTF-8").strip()
-    icloud_status = output
+    icloud_status = output_cmd(check_icloud)
     return icloud_status
     ###
 
 
 def activation_lock_status():
     check_activation_lock = 'system_profiler SPHardwareDataType | awk "/Activation Lock Status/{print $NF}"'
-    proc = subprocess.Popen(check_activation_lock,
-                            shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    output = output.decode("UTF-8").strip()
-    output = output.split(':')
-    activation_lock_status = output[1].strip()
+    activation_lock_status = output_cmd(check_activation_lock)
+    activation_lock_status = activation_lock_status.split(':')[1].strip()
     if activation_lock_status == None:
         activation_lock_status = 'Not Supported'
     return activation_lock_status
     ###
 
 
-def mdm_status():  # TODO
-    check_mdm = 'sudo profiles status -type enrollment'
-    proc = subprocess.Popen(check_mdm, shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    mdm_status = output.decode("UTF-8").strip()
-    mdm_status = mdm_status.splitlines()
+def mdm_status():
     return mdm_status
     ###
 
 
-def dep_status():  # TODO
-    check_dep = 'echo "admin" | sudo profiles renew -type enrollment && sudo profiles show -type enrollment'
-    proc = subprocess.Popen(check_dep, shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = proc.communicate()[0]
-    dep_status = output.decode("UTF-8").strip()
-    print(dep_status)
-    if dep_status == "Error fetching Device Enrollment configuration: Client is not DEP enabled." or output == "(null)":
+def dep_status():
+    check_dep_status = "echo {} | sudo -S profiles renew -type enrollment && sudo profiles show -type enrollment"
+    dep_status = output_cmd(check_dep_status)
+    # if unlocked
+    if dep_status == "Error fetching Device Enrollment configuration: Client is not DEP enabled." or dep_status == "(null)":
         dep_status = 'Unlocked'
+    # if too many requests
     elif dep_status == 'Error fetching Device Enrollment configuration - Request too soon.  Try again later.':
-        print('try again in 5 mins.')
+        dep_status = 'Request too soon from last attempt. Try again later.'
+    # if locked
     else:
         dep_status = 'Locked'
     return dep_status
@@ -95,4 +84,6 @@ def lock_check_json():
     dictionary['DEP Status'] = dep_status()
     JSON = json.dumps(dictionary, indent=4)
     return JSON
-# for commands that require password use encrypted file user can enter password in.
+
+
+print(lock_check_json())
