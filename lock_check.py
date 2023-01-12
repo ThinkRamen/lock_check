bd2b2369960bc8153d86b6bb11414427ae78c567
Alejandro <ThinkRamen@gmail.com>
2023-01-11 20:02:21 -0800

    functions required for lock_check_json completed.

--- lock_check.py
+++ lock_check.py
@@ -5,6 +5,9 @@ default_password = 'admin'
 
 
 def output_cmd(cmd):
+    """
+    returns terminal command using subprocess.Popen, formats the data from bytes to str, and formats it.
+    """
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = proc.communicate()[0]
@@ -13,9 +16,6 @@ def output_cmd(cmd):
 
 
 def serial_number():
-    """
-    returns serial number from system command using subprocess.Popen, formats the data from bytes to str, and formats it.
-    """
     check_serial = "system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'"
     serial_number = output_cmd(check_serial)
     return serial_number
@@ -26,25 +26,30 @@ def fmm_status():
     """
     checks for find my mac.
     """
-    check_fmm = 'nvram -p | awk "/fmm-mobileme-token-FMM-BridgeHasAccount/{print $NF}"'
+    check_fmm = 'nvram -p | awk "/fmm-mobileme-token-FMM-BridgeHasAccount/"'
     fmm_status = output_cmd(check_fmm)
+    fmm_status = fmm_status.split()[1].strip()
     if fmm_status == 'BridgeHasAccountValue':
         fmm_status = True
     else:
-        fmm_status = False
+        fmm_status == False
     return fmm_status
     ###
 
 
-def icloud_status():  # TODO
-    check_icloud = ''
-    icloud_status = output_cmd(check_icloud)
+def icloud_status():
+    check_icloud = 'defaults read MobileMeAccounts Accounts'
+    icloud_status = output_cmd(check_icloud)ch
+    if icloud_status != '(\n)':
+        icloud_status = True
+    if icloud_status == '(\n)':
+        icloud_status = False
     return icloud_status
     ###
 
 
 def activation_lock_status():
-    check_activation_lock = 'system_profiler SPHardwareDataType | awk "/Activation Lock Status/{print $NF}"'
+    check_activation_lock = 'system_profiler SPHardwareDataType | awk "/Activation Lock Status/"'
     activation_lock_status = output_cmd(check_activation_lock)
     activation_lock_status = activation_lock_status.split(':')[1].strip()
     if activation_lock_status == None:
@@ -54,17 +59,26 @@ def activation_lock_status():
 
 
 def mdm_status():
+    check_mdm_status = 'echo {} | sudo -S profiles status -type enrollment'.format(
+        default_password)
+    mdm_status = output_cmd(check_mdm_status)
+    if mdm_status.splitlines()[1].split(':')[1].strip() == 'No':
+        mdm_status = False
+    else:
+        mdm_status = True
     return mdm_status
     ###
 
 
 def dep_status():
-    check_dep_status = "echo {} | sudo -S profiles renew -type enrollment && sudo profiles show -type enrollment".format(
+    check_dep_status = "echo {} | sudo -S profiles renew -type enrollment && sudo -S profiles show -type enrollment".format(
         default_password)
-    dep_status = output_cmd(check_dep_status)
+    proc = subprocess.Popen(check_dep_status, shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    dep_status = proc.communicate()[1].decode().splitlines()
     # if unlocked
     if dep_status == "Error fetching Device Enrollment configuration: Client is not DEP enabled." or dep_status == "(null)":
-        dep_status = 'Unlocked'
+        dep_status = False
     return dep_status
     ###
 
@@ -75,8 +89,8 @@ def lock_check_json():
     dictionary['FMM Enabled'] = fmm_status()
     dictionary['iCloud Enabled'] = icloud_status()
     dictionary['Activation Lock'] = activation_lock_status()
-    dictionary['MDM Status'] = mdm_status()
-    dictionary['DEP Status'] = dep_status()
+    dictionary['MDM Enrollment'] = mdm_status()
+    dictionary['Enrolled Via DEP'] = dep_status()
     JSON = json.dumps(dictionary, indent=4)
     return JSON
 
